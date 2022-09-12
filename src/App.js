43b4ebc038a0576d7de1044c310d9a16e8e94649
rajeev06code <rajeev_sharma15@outlook.com>
2022-09-12 11:20:35 +0530

    react django

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [companyList, setCompanyList] = useState([]);
+  const getCompanyDetails = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/api/v1/company/");
+      setCompanyList(response.data);
+      console.log("the response is===", response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getCompanyDetails();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {companyList.map((data) => (
+        <div className="box">
+          <h3>{data.Company_name}</h3>
+          <div className="box2">
+            <h5>
+              Location:
+              <span style={{ color: "blue", marginLeft: ".5rem" }}>
+                {data.location}
+              </span>
+            </h5>
+            <h5>
+              Type:{" "}
+              <span style={{ color: "blue", marginLeft: ".5rem" }}>
+                {data.type}
+              </span>{" "}
+            </h5>
+          </div>
+          <div>
+            <h5>
+              About:{" "}
+              <span style={{ color: "red", marginLeft: ".5rem" }}>
+                {data.about}
+              </span>
+            </h5>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
